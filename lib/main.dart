919c3045f5964619ee176ecdcaa908e8e5c93fcc
ibrahimmohamed6767 <79427962+ibrahimmohamed6767@users.noreply.github.com>
2022-07-30 16:46:48 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:e_learning/root_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(fontFamily: 'WorkSans'),
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
